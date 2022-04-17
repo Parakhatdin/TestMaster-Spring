45e977f303a16d10145a560e0c331df6cfff4493
Parakhatdin <nuratdinov.p@gmail.com>
2022-04-17 17:59:39 +0500

    first commit

---
+++ src/test/java/uz/popo/testmaster/TestMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.popo.testmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
